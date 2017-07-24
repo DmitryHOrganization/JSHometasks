3e4ff118348528035ac69658abcda6afebf0fd28
Dmitry Hontar <dmitryhontar@gmail.com>
2017-07-24 23:25:58 +0300

    Delete isPalindrome.js

--- isPalindrome.js
+++
@@ -1,9 +0,0 @@
-function isPalindrome(value) {
-if(!isNaN(value) || value===undefined){
-    console.log('Specified parameter is not a valid string')
-}
-else{
-  var valueReverse = value.toString().split('').reverse().join('');
-  return(value===valueReverse);
-}
-}
